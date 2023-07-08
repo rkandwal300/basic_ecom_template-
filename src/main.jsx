1551179221c2573459434c215d4f3eb9c1b9bd8e
Rohit <rahulkandwal707@gmail.com>
2023-07-08 08:13:58 +0530

    Basic  Ecommerce WebSite

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
-import Ani from './Anime/Ani.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <BrowserRouter>
     <App />
-    {/* <Ani/> */}
     </BrowserRouter>
   </React.StrictMode>,
 )
